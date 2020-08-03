df4491a630a19dfd2f0ec54be0d28cb18a933e90
Elcoss <jp14700@gmail.com>
2020-08-03 05:43:00 -0300

    update seeds

--- src/database/seeds/00_create_users.ts
+++ src/database/seeds/00_create_users.ts
@@ -11,8 +11,8 @@ export async function seed(knex:Knex) {
     { nome: "Marcos", email: "marcos", senha: "1234", cpf: 74295136468,matricula: 3470 },
     { nome: "Marcos", email: "marcos2", senha: "1234", cpf: 74295136468,matricula: 3470 },
     { nome: "Marcos", email: "marcos3", senha: "1234", cpf: 74295136468,matricula: 3470 },
-    { nome: "Erricson", email: "erricson", senha: "1234", cpf: 74295136468,matricula: 3470 },
-    { nome: "Erricson", email: "erricson2", senha: "1234", cpf: 74295136468,matricula: 3470 },
-    { nome: "Erricson", email: "erricson3", senha: "1234", cpf: 74295136468,matricula: 3470 },
+    { nome: "Erricsson", email: "erricsson", senha: "1234", cpf: 74295136468,matricula: 3470 },
+    { nome: "Erricsson", email: "erricsson2", senha: "1234", cpf: 74295136468,matricula: 3470 },
+    { nome: "Erricsson", email: "erricsson3", senha: "1234", cpf: 74295136468,matricula: 3470 },
   ])
 }
