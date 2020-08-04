00dc9ca5a84ffbaee51d5bfc138f22b88e229acd
Elcoss <jp14700@gmail.com>
2020-08-04 05:15:43 -0300

    add photo route

--- src/controllers/PhotoController.ts
+++ src/controllers/PhotoController.ts
@@ -2,12 +2,21 @@ import {Request, Response} from 'express'
 import knex from '../database/connection'
 
 interface Photo {
+  fieldname: string;
+  originalname: string;
+  encoding: string;
+  mimetype: string;
+  size: number;
+  destination: string;
+  filename: string;
+  path: string;
+  buffer: Buffer;
   location: String
 }
 class PhotoController {
   async update(request: Request, response: Response) {
     const { id } = request.params
-    const { location } = request.file as Photo
+    const { location } = request.file as unknown as Photo
 
 
 
