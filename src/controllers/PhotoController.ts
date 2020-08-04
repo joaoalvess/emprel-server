03d566c41177fab19ebecde545ff0ebe3627416e
Elcoss <jp14700@gmail.com>
2020-08-04 14:13:23 -0300

    config heroku

--- src/controllers/PhotoController.ts
+++ src/controllers/PhotoController.ts
@@ -8,11 +8,9 @@ class PhotoController {
   async update(request: Request, response: Response) {
     const { id } = request.params
 
-    const { location } = request.file as unknown as Photo
+    const file = request.file
 
-    await knex('users').where('id', id).update({ url: location })
-
-    return response.json({messager: "sucess"})
+    return response.json(file)
   }
 }
 
