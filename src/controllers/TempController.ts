1ec2a5c887e046d831fa9c1bfc4120d9e0d2b1ab
Elcoss <jp14700@gmail.com>
2020-08-08 18:14:24 -0300

    temp controller update

--- src/controllers/TempController.ts
+++ src/controllers/TempController.ts
@@ -4,9 +4,14 @@ import knex from '../database/connection'
 class TempController {
   async update(request: Request, response: Response) {
     const { id } = request.params
-    const { temperatura } = request.body
+    const { temperatura, apto } = request.body
 
-    await knex('forms').where('id', id).update({ temperatura: temperatura })
+    if(apto == true) {
+      await knex('forms').where('id', id).update({ temperatura: temperatura })
+    }
+    if(apto == false) {
+      await knex('forms').where('id', id).update({ temperatura: temperatura, apto: apto })
+    }
 
     return response.json({messager: "temperatura atualizada"})
   }
