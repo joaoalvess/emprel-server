74d85b69cbb8a62e8f3b5b3973440263a9ebbe1d
João Alves <git>
2020-08-12 08:22:53 -0300

    edit query

--- src/controllers/FormsController.ts
+++ src/controllers/FormsController.ts
@@ -23,55 +23,55 @@ class FormsController{
 
     switch (select) {
       case "data":
-        const filterData = await knex('forms').where('data', String(data))
+        const filterData = await knex('forms').where('data', String(selectDate))
         response.json(filterData)
       break
       case "infectado":
-        const filterInfectado = await knex('forms').where('infectado', Boolean(infectado)).where('date', String(selectDate))
+        const filterInfectado = await knex('forms').where('infectado', Boolean(infectado)).where('data', String(selectDate))
         response.json(filterInfectado)
       break
       case "contato_infectado":
-        const filterContato = await knex('forms').where('contato_infectado', Boolean(contato_infectado)).where('date', String(selectDate))
+        const filterContato = await knex('forms').where('contato_infectado', Boolean(contato_infectado)).where('data', String(selectDate))
         response.json(filterContato)
       break
       case "tosse":
-        const filterTosse = await knex('forms').where('tosse', Boolean(tosse)).where('date', String(selectDate))
+        const filterTosse = await knex('forms').where('tosse', Boolean(tosse)).where('data', String(selectDate))
         response.json(filterTosse)
       break
       case "febre":
-        const filterFebre = await knex('forms').where('febre', Boolean(febre)).where('date', String(selectDate))
+        const filterFebre = await knex('forms').where('febre', Boolean(febre)).where('data', String(selectDate))
         response.json(filterFebre)
       break
       case "falta_ar":
-        const filterFalta_ar= await knex('forms').where('falta_ar', Boolean(falta_ar)).where('date', String(selectDate))
+        const filterFalta_ar= await knex('forms').where('falta_ar', Boolean(falta_ar)).where('data', String(selectDate))
         response.json(filterFalta_ar)
       break
       case "calafrio":
-        const filterCalafrio= await knex('forms').where('calafrio', Boolean(calafrio)).where('date', String(selectDate))
+        const filterCalafrio= await knex('forms').where('calafrio', Boolean(calafrio)).where('data', String(selectDate))
         response.json(filterCalafrio)
       break
       case "garganta":
-        const filterGarganta = await knex('forms').where('garganta', Boolean(garganta)).where('date', String(selectDate))
+        const filterGarganta = await knex('forms').where('garganta', Boolean(garganta)).where('data', String(selectDate))
         response.json(filterGarganta)
       break
       case "cabeça":
-        const filterCabeça = await knex('forms').where('cabeça', Boolean(cabeça)).where('date', String(selectDate))
+        const filterCabeça = await knex('forms').where('cabeça', Boolean(cabeça)).where('data', String(selectDate))
         response.json(filterCabeça)
       break
       case "corpo":
-        const filterCorpo = await knex('forms').where('corpo', Boolean(corpo)).where('date', String(selectDate))
+        const filterCorpo = await knex('forms').where('corpo', Boolean(corpo)).where('data', String(selectDate))
         response.json(filterCorpo)
       break
       case "olfato":
-        const filterOlfato = await knex('forms').where('olfato', Boolean(olfato)).where('date', String(selectDate))
+        const filterOlfato = await knex('forms').where('olfato', Boolean(olfato)).where('data', String(selectDate))
         response.json(filterOlfato)
       break
       case "paladar":
-        const filterPaladar = await knex('forms').where('paladar', Boolean(paladar)).where('date', String(selectDate))
+        const filterPaladar = await knex('forms').where('paladar', Boolean(paladar)).where('data', String(selectDate))
         response.json(filterPaladar)
       break
       case "temperatura":
-        const filterTemperatura = await knex('forms').where('temperatura', '>=', 37.2).where('date', String(selectDate))
+        const filterTemperatura = await knex('forms').where('temperatura', '>=', 37.2).where('data', String(selectDate))
         response.json(filterTemperatura)
       break
       default:
