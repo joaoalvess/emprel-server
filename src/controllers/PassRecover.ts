65416d4178f76c1cb57f2f285231bcd78cb09bb5
Elcoss <jp14700@gmail.com>
2020-08-17 00:53:56 -0300

    route update

--- src/controllers/PassRecover.ts
+++ src/controllers/PassRecover.ts
@@ -1,20 +1,23 @@
 import { Request, Response } from 'express'
 import knex from '../database/connection'
-
+import nodemailer from 'nodemailer'
 class PassRecover {
   async show(request: Request, response: Response) {
     const { email, cpf } = request.body
 
-    const user = await knex('users').where('email', email).where('cpf', cpf).first()
+    const user = process.env.USER
+    const pass = process.env.PASS
 
-    if(!user){
+    const selectUser = await knex('users').where('email', email).where('cpf', cpf).first()
+
+    if(!selectUser){
       return response.json({messager: "usuario não encontrado"})
     }
 
     var crypto = require("crypto");
     var code = crypto.randomBytes(3).toString('hex');
 
-    response.json(code)
+    response.json({user,pass})
   }
 }
 
