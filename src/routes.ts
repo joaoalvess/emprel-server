640f7040945578bb66cd2ab00c05e537e5780c50
Elcoss <jp14700@gmail.com>
2020-08-20 19:08:25 -0300

    passreset send

--- src/routes.ts
+++ src/routes.ts
@@ -37,7 +37,7 @@ routes.put("/updateusermatricula/:id", usersController.updateMatricula);
 routes.get("/perfil/:id", perfilController.show);
 routes.put("/perfil/:id", perfilController.update);
 
-routes.get("/send", passRecover.show);
+routes.post("/send", passRecover.show);
 
 routes.put("/formtemp/:id", tempController.update);
 
